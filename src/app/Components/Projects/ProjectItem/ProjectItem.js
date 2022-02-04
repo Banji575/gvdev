45728b2b0f70fef5567cf3625a0ae917583e5232
Sergey <banjamin@yandex.ru>
2022-02-04 17:56:17 +0300

    add image component

--- src/app/Components/Projects/ProjectItem/ProjectItem.js
+++ src/app/Components/Projects/ProjectItem/ProjectItem.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { ImageWrapper } from "../../ImageWrapper/ImageWrapper.tsx";
+import image1 from './../../../img/game1.jpg'
 import './ProjectItem.scss'
 
 export const ProjectItem = () => {
     return(
         <div className="ProjectItem">
-              
+              <div className="ProjectItem-img">
+
+              </div>
+              <div className="ProjectItem-descript">
+                    {/* <img src={image1} alt="" srcset="" /> */}
+                    <ImageWrapper className="" src={image1}/>
+              </div>
         </div>
     )
 }
